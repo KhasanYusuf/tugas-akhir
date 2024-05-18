e8ce3c66afd80e8b8b2d1f12f32937500209a3c9
Khoirudinn <113275326+Khoirudinn@users.noreply.github.com>
2024-05-18 21:44:17 +0700

    tambah resources/views

---
+++ anemia_prediction/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('navbar')
+
+@section('konten')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anemia Prediction</title>
+    <link rel="stylesheet" href="css/new_home.css">
+    <link rel="stylesheet" href="css/logreg.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+
+</head>
+
+<body>
+ HOME
+</body>
+
+</html>
+
+
+@endsection
--- anemia_prediction/routes/web.php
+++ anemia_prediction/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/beranda', function () {
+    return view('home');
+});
+Route::get('/prediksi', function () {
+    return view('predict');
+});
+Route::get('/data-latih', function () {
+    return view('data');
+});
+Route::get('/hasil', function () {
+    return view('output');
 });
